e676a51e3307815189dfcbc4027c8e95e3965ca0
alien-wxr <erick-wang@outlook.com>
2019-02-28 18:33:41 +0800

    alpha

---
+++ CamelStudio2019/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include "Qsci/qsciscintilla.h"
+#include "Qsci/qscilexercpp.h"
+#include "Qsci/qsciapis.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
